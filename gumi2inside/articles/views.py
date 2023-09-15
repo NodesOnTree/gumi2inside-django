d3011f137c0a56895e796885eb7972985b3bbbf0
hmy <hmy940424@naver.com>
2023-09-15 21:44:49 +0900

    수정

--- gumi2inside/articles/views.py
+++ gumi2inside/articles/views.py
@@ -5,34 +5,30 @@ from .models import Article, Comment
 def index(request):
     return render(request, "articles/index.html")
 
-def new(request):
+def create(request):
+    # title = request.POST.get('title')
+    # content = request.POST.get('content')
+    # article = Article(title=title, content=content)
+    # article.save()
     return render(request, 'articles/create.html')
 
-def create(request):
+def watch_articles(request):
     title = request.POST.get('title')
     content = request.POST.get('content')
-    article = Article(title=title,content=content)
+    article = Article(title=title, content=content)
     article.save()
-    return redirect('articles:index')
+    articles = Article.objects.all()
+    comments = Comment.objects.all()
 
-def complete(request):
-    return render(request, 'articles/complete.html')
-
-def watch_articles(request):
-    article = Article.objects.all()
-    comment = Comment.objects.all()
-
-    context={
-        'articles' : article,
-        'comments' : comment,
+    context = {
+        'articles': articles,
+        'comments': comments,
     }
     return render(request, "articles/watch_articles.html", context)
 
 def list(request):
-
     articles = Article.objects.all()
-    # index 템플릿에서 사용할 수 있도록 전달
     context = {
         'articles': articles,
     }
-    return render(request, 'articles/list.html',context)
+    return render(request, 'articles/list.html', context)
