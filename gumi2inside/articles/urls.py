6192fa4e67f30b67e09304b469f691e561035d39
성규 <sgbae777@gmail.com>
2023-09-15 17:23:44 +0900

    comit

--- gumi2inside/articles/urls.py
+++ gumi2inside/articles/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'articles'
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('watch_articles/', views.watch_articles, name='watch_articles'),
 ]
--- gumi2inside/articles/views.py
+++ gumi2inside/articles/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Article, Comment
 
 # Create your views here.
 def index(request):
     return render(request, "articles/index.html")
+
+
+def watch_articles(request):
+    article = Article.objects.all()
+    comment = Comment.objects.all()
+
+    context={
+        'articles' : article,
+        'comments' : comment,
+    }
+    return render(request, "articles/watch_articles.html", context)
