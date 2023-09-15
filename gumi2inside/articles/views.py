617ebd1f2a7bf491b8893f200b9a671a414c389b
yoohyungmin <hmy940424@naver.com>
2023-09-15 16:58:15 +0900

    create 함수,html 파일 추가

--- gumi2inside/articles/views.py
+++ gumi2inside/articles/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "articles/index.html")
+
+def create(request):
+    return render(request, "articles/create.html")
