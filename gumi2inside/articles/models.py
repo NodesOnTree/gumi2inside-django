53c6157c2b54413cb621f00be6de1ce0207dea28
= <=>
2023-09-15 16:47:00 +0900

    Models 정의 - Article, Comment

--- gumi2inside/articles/models.py
+++ gumi2inside/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    
+
+class Comment(models.Model):
+    content = models.TextField()
+    origin_article = models.ForeignKey('Article', on_delete=models.CASCADE)
