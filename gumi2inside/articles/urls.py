b65e39f6ac3758431b763c600058421f371f015a
yoohyungmin <hmy940424@naver.com>
2023-09-15 17:36:20 +0900

    새로운 함수 new, complete 추가 html 추가

--- gumi2inside/articles/urls.py
+++ gumi2inside/articles/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('create/', views.create, name="create")
+    path('create/', views.create, name="create"),
+    path('new/', views.new, name="new"),
+    path('complete/', views.complete, name="complete"),
 ]
--- gumi2inside/articles/views.py
+++ gumi2inside/articles/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Article
 # Create your views here.
 def index(request):
     return render(request, "articles/index.html")
 
+def new(request):
+    return render(request, 'articles/create.html')
+
 def create(request):
-    return render(request, "articles/create.html")
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    article = Article(title=title,content=content)
+    article.save()
+    return redirect('articles:index')
+
+def complete(request):
+    return render(request, 'articles/complete.html')
