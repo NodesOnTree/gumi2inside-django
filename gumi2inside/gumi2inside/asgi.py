2a78d0a176728e4252d47588dc767d1f535539b3
= <=>
2023-09-15 16:15:27 +0900

    first commitgit add .

---
+++ gumi2inside/gumi2inside/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gumi2inside project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gumi2inside.settings')
+
+application = get_asgi_application()
