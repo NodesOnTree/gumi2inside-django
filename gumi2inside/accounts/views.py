1397f1a1ad7408e2189eb521c2ab0fdd2e1eaed1
seom <dltjdah25@naver.com>
2023-10-09 05:31:59 +0900

    sm

--- gumi2inside/accounts/views.py
+++ gumi2inside/accounts/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+
 def login(request):
     if request.method == "POST":
         form = AuthenticationForm(request, request.POST)
@@ -21,6 +23,32 @@ def login(request):
     }
     return render(request,'accounts/login.html',context)
 
+
 def logout(request):
     auth_logout(request)
     return redirect('home') 
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            print(request.POST.get('first_name'))
+            print(request.POST.get('last_name'))
+            print("POST 데이터:", request.POST)
+            print("클린 데이터:", form.cleaned_data)
+            print("first_name:", form.cleaned_data.get('first_name'))
+            print("last_name:", form.cleaned_data.get('last_name'))
+            
+            form.save()
+            return redirect('/')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+def delete(request):
+    request.user.delete()
+    return redirect('/')
--- gumi2inside/challenges/urls.py
+++ gumi2inside/challenges/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 from . import views
 
-app_name = "ciders"
+app_name = "challenges"
 urlpatterns = [
-    path("", views.ciders_home , name="main"),
-    path("create/", views.create_bubble, name="create"),
-    path("<int:bubble_id>/like/", views.like_bubble, name="like_bubble"),
-    path("<int:bubble_id>/dislike/", views.dislike_bubble, name="dislike_bubble"),
+    # 챌린지 메인 화면, 챌린지 리스트 나옴
+    path("", views.challenges_home , name="main"),
+    # 새로운 챌린지 생성
+    path("create/", views.create_challenge, name="create"),
+    # 챌린지 삭제
+    path("delete/", views.delete_challenge, name="delete"),
+    # 개별 챌린지 확인
+    path("detail/", views.detail_challenge, name="detail"),
+    # 개별 챌린지 등록
+    path("new/", views.new_detail, name="new"),
+    # 개별 챌린지 삭제
+    path("del/", views.del_detail, name="del"),
+
+
+    # 챌린지 정보 변경
 ]
